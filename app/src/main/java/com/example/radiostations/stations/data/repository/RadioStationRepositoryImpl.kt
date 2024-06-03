4b267d6417d244997c76255464d94f30eac8c234
Debin Tom <debintom.software@gmail.com>
2024-06-03 18:20:39 +0100

    repository unit tests added

--- app/src/main/java/com/example/radiostations/stations/data/repository/RadioStationRepositoryImpl.kt
+++ app/src/main/java/com/example/radiostations/stations/data/repository/RadioStationRepositoryImpl.kt
@@ -1,16 +1,44 @@
 package com.example.radiostations.stations.data.repository
 
+import coil.network.HttpException
 import com.example.radiostations.core.utils.Resource
+import com.example.radiostations.stations.data.model.RadioStationItem
 import com.example.radiostations.stations.domain.model.RadioStationEntity
 import com.example.radiostations.stations.domain.repository.RadioStationRepository
 import com.example.radiostations.stations.framework.apiservice.RadioStationApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import okio.IOException
 
 
 class RadioStationRepositoryImpl(private val apiService: RadioStationApiService) : RadioStationRepository {
+     private var cachedStations: List<RadioStationEntity>? = null
+
     override fun getStations(offset: Int, limit: Int): Flow<Resource<List<RadioStationEntity>>> = flow {
         emit(Resource.Loading)
-        emit(Resource.Success(emptyList()))
+        try {
+            if(cachedStations == null) {
+                val response = apiService.getAllStations()
+                cachedStations = response.map {
+                    station -> station.toRadioStationEntity()
+                }
+            }
+            val paginatedStations = cachedStations!!.drop(offset).take(limit)
+            emit(Resource.Success(paginatedStations))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+
+    private fun RadioStationItem.toRadioStationEntity(): RadioStationEntity {
+        return RadioStationEntity(
+            name = this.name,
+            state = this.state,
+            country = this.country,
+            countrycode = this.countrycode,
+            stationuuid = this.stationuuid
+        )
     }
 }
--- app/src/test/java/com/example/radiostations/stations/data/repository/RadioStationRepositoryImplTest.kt
+++ app/src/test/java/com/example/radiostations/stations/data/repository/RadioStationRepositoryImplTest.kt
@@ -8,8 +8,10 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.test.runTest
+import okio.IOException
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class RadioStationRepositoryImplTest {
@@ -25,16 +27,60 @@ class RadioStationRepositoryImplTest {
 
     @Test
     fun getStations_should_fetch_and_cache_station() = runTest {
-       val stations = listOf(
+       val mockStations = listOf(
            RadioStationItem(name = "Station1", state = "State1", country = "Country1", countrycode = "1", stationuuid = "id1"),
            RadioStationItem(name = "Station2", state = "State2", country = "Country2", countrycode = "2", stationuuid = "id2"),
        )
         coEvery {
             apiService.getAllStations()
-        }returns stations
+        } returns mockStations
 
          val flow = repository.getStations(0, 2).toList()
          assertTrue { flow[0] is Resource.Loading }
          assertTrue { flow[1] is Resource.Success }
+
+        val stations = (flow[1] as Resource.Success).data ?: emptyList()
+        assertEquals(2, stations.size)
+        assertEquals("Station1", stations[0].name)
+        assertEquals("Station2", stations[1].name)
+        assertEquals("State1", stations[0].state)
+        assertEquals("State2", stations[1].state)
+        assertEquals("Country1", stations[0].country)
+        assertEquals("Country2", stations[1].country)
+        assertEquals("1", stations[0].countrycode)
+        assertEquals("2", stations[1].countrycode)
+        assertEquals("id1", stations[0].stationuuid)
+        assertEquals("id2", stations[1].stationuuid)
+    }
+
+    @Test
+    fun getStations_should_return_paginated_stations() = runTest {
+        val mockStations = List(100) {index ->
+            RadioStationItem(name = "Station$index", state = "State$index", country = "Country$index", countrycode = "CountryCode$index", stationuuid = "StationId$index")
+        }
+        coEvery { apiService.getAllStations() } returns mockStations
+
+        val flow = repository.getStations(0, 10).toList()
+        assertTrue { flow[0] is Resource.Loading }
+        assertTrue { flow[1] is Resource.Success }
+        val stations = (flow[1] as Resource.Success).data?.take(10) ?: emptyList()
+        assertEquals(10, stations.size)
+        assertEquals("Station0", stations[0].name)
+
+        val nextFlow = repository.getStations(10,10).toList()
+        assertTrue { nextFlow [0] is Resource.Loading }
+        assertTrue { nextFlow[1] is Resource.Success }
+        val nextStations = (nextFlow[1] as Resource.Success).data?.take(10) ?: emptyList()
+        assertEquals(10, nextStations.size)
+        assertEquals("Station10", nextStations[0].name)
+    }
+
+    @Test
+    fun getStations_should_return_error_when_api_fails() = runTest {
+        coEvery { apiService.getAllStations() } throws IOException()
+        val stations = repository.getStations(0,10).toList()
+        assertTrue { stations[0] is Resource.Loading }
+        assertTrue { stations[1] is Resource.Error }
+        assertEquals((stations[1] as Resource.Error).message, "Couldn't reach server. Check your internet connection")
     }
 }
